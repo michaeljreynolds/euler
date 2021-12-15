df10ee7fda8032f96cdd190d3e1585c99fc93c5a
michaeljreynolds <migomick.compsci@gmail.com>
2021-12-15 10:41:06 -0500

    Sum even numbered fib terms up to a limit
    Redid this problem, iterative of course. 4 million is too big a limit to do this recursively. Extremely fast and easy to read.

--- problem1.js
+++ problem1.js
@@ -3,6 +3,10 @@ If we list all the natural numbers below 10 that are multiples of 3 or 5, we get
 Find the sum of all the multiples of 3 or 5 below 1000.
 */
 
+
+// This is probably one of the cleanest solutions
+// The 3 and 5 magic numbers are only written once
+
 // global value for multiples
 const multiplesOf = [3,5];
 
@@ -13,6 +17,9 @@ function solve(limit) {
 
 
 function findMultiples(limit, multiplesOf) {
+    // by using an object, we save our multiple value as a property of an object
+    // this is an indirect hash
+    // we can then loop through the objects properties values and sum
     let multiples = {};
     for (let i = 0; i < multiplesOf.length; i++) {
         const startNum = multiplesOf[i];
